e175df28f76c52949e38419a826b92da12ff2aa6
bergonzit <tbergonzi@frba.utn.edu.ar>
2026-04-08 22:49:15 -0300

    Otra vez

--- Keydrop/keydrop.py
+++ Keydrop/keydrop.py
@@ -9,13 +9,20 @@ def esperar(tiempo):
     time.sleep(tiempo)
 
 def clickear(posX,posY):
+    esperar(tiempoEsperaClick/2)
     pyautogui.click(x = posX, y = posY)
-    esperar(tiempoEsperaClick)
+    esperar(tiempoEsperaClick/2)
 
+
+cantidadRepeticiones = input("Cantidad de repeticiones: ")
+desvioX = input("Desvio Sorteo X: ")
+desvioY = input("Desvio Sorteo Y: ")
+entrarX = input("Desvio Entrar Sorteo X: ")
+entrarY = input("Desvio Entrar Sorteo Y: ")
 for i in range(cantidadRepeticiones):
     clickear(280,100) #Click en barra de marcadores
-    clickear(1100,512) #Click en sorteo
-    clickear(1150,500) #Click entrar al sorteo
+    clickear(1100 + desvioX,512 + desvioY) #Click en sorteo
+    clickear(1150 + entrarX,500 + entrarY) #Click entrar al sorteo
     print("Ciclo ", (i+1), " de ", cantidadRepeticiones)
     esperar(tiempoEsperaSorteo) #Espera el tiempo restante
 exit()
